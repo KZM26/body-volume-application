a0f3efd513a10f60fbfca88a787880b61f0dc7ba
Khwezi Majola <khwezimajola@gmail.com>
2019-02-04 15:11:37 +0200

    Implemented a process to extract the reference shape PIDs from the reference landmark points

--- src/main/scala/preprocessor/preprocessor.scala
+++ src/main/scala/preprocessor/preprocessor.scala
@@ -1,9 +1,19 @@
 package preprocessor
 
-import scalismo.io.MeshIO
 import java.io.File
+import java.io.PrintWriter
+import java.nio.file.{Files, Paths}
 
+import javafx.scene.shape.TriangleMesh
+import scalismo.common.PointId
+import scalismo.io.MeshIO
 import scalismo.ui.api.ScalismoUI
+import scalismo.io._
+import scalismo.geometry._
+import scalismo.mesh.TriangleMesh3D
+import scalismo.registration._
+import scalismo.statisticalmodel.dataset.{DataCollection, DataItem}
+import scala.collection.mutable.ListBuffer
 
 class preprocessor {
 
@@ -21,7 +31,7 @@ class preprocessor {
           this.landmarkRef()
 
         case "a" => // Start Alignment
-        // TODO
+          this.align()
 
         case "h" => // Help
           println("Learn how to use a computer you scrub\n")
@@ -43,26 +53,97 @@ class preprocessor {
     }
   }
 
-    def landmarkRef(): Unit = {
+  def landmarkRef(): Unit = {
 
-      // Initialise Scalismo
-      scalismo.initialize()
+    // Initialise Scalismo
+    scalismo.initialize()
 
-      println("Scalismo Viewer will now open\nSelect the landmarks and save them")
+    println("Scalismo Viewer will now open\nSelect the landmarks and save them as refLandmarks.json in the data directory")
 
-      // create a visualisation window
-      val ui = ScalismoUI()
+    // create a visualisation window
+    val ui = ScalismoUI()
 
-      // Load the reference shape
-      val mesh = MeshIO.readMesh(new File("data/training/tr_gt_000.stl")).get
+    // Load the reference shape
+    val mesh = MeshIO.readMesh(new File("data/training/tr_gt_000.stl")).get
 
-      // Display it
-      val meshView = ui.show(mesh, "reference")
+    // Display it
+    val group = ui.createGroup("object-1")
+    val meshView = ui.show(group, mesh, "body_0")
 
-      // val files = new File("data/training/").listFiles
-      // val dataset = files.map{f => MeshIO.readMesh(f).get}
-      // (0 until dataset.size).foreach{i => show(dataset(i),"body_"+i)}
+    // val files = new File("data/training/").listFiles
+    // val dataset = files.map{f => MeshIO.readMesh(f).get}
+    // (0 until dataset.size).foreach{i => show(dataset(i),"body_"+i)}
+
+  }
+
+  def align(): Unit = {
+
+    // Check if refLandmarks.json exists - Landmark file
+    if (!Files.exists(Paths.get("data/ref_landmarks/refLandmarks.json"))) {
+      println("Landmark file - refLandmarks.json not found. Returning to previous menu")
+      return
+    }
+
+    // If exists, start alignment
+    // Initialise Scalismo
+    scalismo.initialize()
+
+    println("Scalismo initialised")
+
+    // First load files
+    val files = new File("data/training/").listFiles
+    val dataset = files.map{f => MeshIO.readMesh(f).get}
+
+    println("Data loaded")
+
+    // The reference landmarks are based on shape 0 in the training set
+    val refLandmarks = LandmarkIO.readLandmarksJson[_3D](new File("data/ref_landmarks/refLandmarks.json")).get
+    val reference = dataset.head
+
+    // Extract the point IDs from the reference shape using the landmark coordinates
+    // First check if file exists
+    if (!Files.exists(Paths.get("data/ref_landmarks/refLandmarksPoints.txt"))) {
+      println("Landmark point file not found\nRunning point finder. This can take a while")
+      // Reference landmark iterator
+      // List buffer for points
+      val it = refLandmarks.seq.iterator
+      var points = new ListBuffer[Int]()
+
+      // Iterate, get point, extract pid from the reference shape
+      while (it.hasNext){
+        val pt = it.next().point
+        val pid = reference.pointSet.pointId(pt)
+        points += pid.get.id
+      }
+
+      // Write to file
+      val writer = new PrintWriter(new File("data/ref_landmarks/refLandmarksPoints.txt"))
+      writer.write(points.mkString("\n"))
+      writer.close()
 
     }
 
+
+    // Get the shapes to align to the reference
+    val toAlign = dataset.tail
+    // Get the point IDs of the landmarks
+    val pointIDs = refLandmarks.map(_.id)
+
+    println("Reference landmarks extracted")
+
+    println("Starting alignment")
+    // Find a rigid transform for each in the to align set and apply it
+    val alignedSet = dataset.map{ mesh =>
+      val landmarks = pointIDs.map{id => Landmark[_3D]("L_"+id, mesh.pointSet.point(PointId(id.toInt)))}
+      // TODO: Figure out what the centre should actually be
+      val rigidTrans = LandmarkRegistration.rigid3DLandmarkRegistration(landmarks, refLandmarks, Point3D(0, 0, 0))
+      mesh.transform(rigidTrans)
+    }
+
+    println("Alignment complete. Writing to file")
+    // Write the aligned data to file
+    Array.tabulate(alignedSet.length){i => MeshIO.writeMesh(alignedSet(i), new File("data/training_aligned/tr_gt0" + i.toString + "0.stl"))}
+    println("Writing complete")
+
+  }
 }
