15dd6d1280299869de997e35ca0ebf288761afac
Khwezi Majola <khwezimajola@gmail.com>
2019-04-08 17:24:12 +0200

    Changed sphere volume threshold to 99%. Changed arbitrary iterable to indexed seq and operations to map.

--- src/test/scala/measurement/VolumeTest.scala
+++ src/test/scala/measurement/VolumeTest.scala
@@ -4,13 +4,12 @@ import java.io.File
 
 import measurement.Measurement.getMeshVolume
 import test.BVATestSuite
-import org.scalatest.FunSuite
 import scalismo.io.MeshIO
 
 import scala.collection.mutable.ListBuffer
 import scala.io.Source
 
-class VolumeTest extends BVATestSuite{
+class VolumeTest extends BVATestSuite {
 
   it("Cube volume measurements exactly equal") {
 
@@ -26,7 +25,7 @@ class VolumeTest extends BVATestSuite{
     }
   }
 
-  it ("Sphere volume within 95% of expected value"){
+  it ("Sphere volume within 99% of expected value"){
 
     val spFiles = new File("data/volume-test/").listFiles.filter(f => f.getName.contains("sp")).filter(f => f.getName.contains(".stl")).sortBy(_.getName)
     val spTrueVolume =  spFiles.map{f => (4.0/3.0) * math.Pi * math.pow(f.getName.substring(0, f.getName.indexOf(".") - 2).toInt, 3)}
@@ -34,7 +33,7 @@ class VolumeTest extends BVATestSuite{
 
     for (i <- spDataset.indices){
       val mesh = spDataset(i)
-      assert(math.abs(getMeshVolume(mesh)/spTrueVolume(i) - 1) < 0.05)
+      assert(math.abs(getMeshVolume(mesh)/spTrueVolume(i) - 1) < 0.01)
     }
   }
 
@@ -44,12 +43,11 @@ class VolumeTest extends BVATestSuite{
     val arDataset = arFiles.map{f => MeshIO.readMesh(f).get}
     val volFile = Source.fromFile("data/volume-test/volume.txt")
     // Read line by line using iterator. Drop first two lines
-    val iter = volFile.getLines()
+    val iter = volFile.getLines().toIndexedSeq
 
     // Extract shape name and volume line by line
     // Line Format: name-volume
-    while (iter.hasNext){
-      val info = iter.next()
+    iter.map{ info =>
       val shapeName = info.substring(0, info.indexOf("-"))
       val shapeVolume = info.substring(info.indexOf("-") + 1, info.length).toDouble
       val mesh = MeshIO.readMesh(arFiles.filter(p => p.getName.containsSlice(shapeName)).head).get
